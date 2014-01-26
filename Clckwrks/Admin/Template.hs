4a5f875b54a82b1989c540f461184df0bc433042
Jeremy Shaw <jeremy@seereason.com>
2014-01-26 19:29:10 +0000

    clckwrks: added emptyTemplate

--- Clckwrks/Admin/Template.hs
+++ Clckwrks/Admin/Template.hs
@@ -55,6 +55,49 @@ template title headers body = do
      </body>
     </html>)
 
+emptyTemplate ::
+    ( Happstack m
+    , EmbedAsChild (ClckT url m) headers
+    , EmbedAsChild (ClckT url m) body
+    ) => String -> headers -> body -> ClckT url m Response
+emptyTemplate title headers body = do
+   siteName <- (fromMaybe "Your Site") <$> query GetSiteName
+   toResponse <$> (unXMLGenT $
+    <html>
+     <head>
+      <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap.min.css"        rel="stylesheet" media="screen" />
+      <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap-responsive.css" rel="stylesheet" />
+      <link type="text/css" href="/static/admin.css" rel="stylesheet" />
+      <script type="text/javascript" src="/jquery/jquery.js" ></script>
+      <script type="text/javascript" src="/json2/json2.js" ></script>
+      <script type="text/javascript" src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/js/bootstrap.min.js" ></script>
+
+      <title><% title %></title>
+      <% headers %>
+     </head>
+     <body>
+      <div class="navbar">
+       <div class="navbar-inner">
+        <div class="container-fluid">
+         <div class="brand"><% siteName %></div>
+        </div>
+       </div>
+      </div>
+
+      <div class="container-fluid">
+       <div class="row-fluid">
+        <div class="span2">
+--         <% sidebar %>
+        </div>
+        <div class="span10">
+         <% body %>
+        </div>
+       </div>
+      </div>
+     </body>
+    </html>)
+
+
 sidebar :: (Happstack m) => XMLGenT (ClckT url m) XML
 sidebar = adminMenuXML
 
