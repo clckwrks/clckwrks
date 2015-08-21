2ee5efd03da3fcf62fa25630feadf6f01935a711
Jeremy Shaw <jeremy@n-heptane.com>
2015-08-21 00:04:06 -0500

    call up-login instead of up-login-inline. Fix so that forgotten password shows up when your are not authenticated instead of when you are authenticated.

--- Clckwrks/Authenticate/Page/Login.hs
+++ Clckwrks/Authenticate/Page/Login.hs
@@ -17,18 +17,17 @@ loginPage =
       <div ng-controller="UsernamePasswordCtrl">
        <div up-authenticated=False>
         <h2>Login</h2>
-        <up-login-inline />
-       </div>
-
-       <div up-authenticated=True>
-
-        <h2>Logout</h2>
-        <p>You have successfully logged in! Click the link below to logout.</p>
-        <up-logout />
+        <up-login />
 
         <h2>Forgotten Password?</h2>
         <p>Forgot your password? Request a reset link via email!</p>
         <up-request-reset-password />
+       </div>
+
+       <div up-authenticated=True>
+        <h2>Logout</h2>
+        <p>You have successfully logged in! Click the link below to logout.</p>
+        <up-logout />
        </div>
 
        <h2>Create A New Account</h2>
