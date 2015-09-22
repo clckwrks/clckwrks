b390d67a4718dd0ac6ea4047e084ab5e1260001e
Jeremy Shaw <jeremy@n-heptane.com>
2015-09-22 16:53:51 -0500

    update .nix files

--- default.nix
+++ default.nix
@@ -5,27 +5,26 @@
 , ixset, jmacro, lens, mtl, network, network-uri, old-locale
 , openssl, process, random, reform, reform-happstack, reform-hsp
 , safecopy, stdenv, stm, tagsoup, text, time, time-locale-compat
-, unordered-containers, utf8-string, uuid, vector, web-plugins
-, web-routes, web-routes-happstack, web-routes-hsp, web-routes-th
-, xss-sanitize, userid
+, unordered-containers, userid, utf8-string, uuid-orphans
+, uuid-types, vector, web-plugins, web-routes, web-routes-happstack
+, web-routes-hsp, web-routes-th, xss-sanitize
 }:
 mkDerivation {
   pname = "clckwrks";
-  version = "0.23.6";
+  version = "0.23.10";
   src = ./.;
-  buildDepends = [
+  libraryHaskellDepends = [
     acid-state aeson aeson-qq attoparsec base blaze-html bytestring
     cereal containers directory filepath happstack-authenticate
     happstack-hsp happstack-jmacro happstack-server
     happstack-server-tls hsp hsx-jmacro hsx2hs ixset jmacro lens mtl
     network network-uri old-locale process random reform
     reform-happstack reform-hsp safecopy stm tagsoup text time
-    time-locale-compat unordered-containers utf8-string uuid vector
-    web-plugins web-routes web-routes-happstack web-routes-hsp
-    web-routes-th xss-sanitize userid
+    time-locale-compat unordered-containers userid utf8-string
+    uuid-orphans uuid-types vector web-plugins web-routes
+    web-routes-happstack web-routes-hsp web-routes-th xss-sanitize
   ];
-  buildTools = [ hsx2hs ];
-  extraLibraries = [ openssl ];
+  librarySystemDepends = [ openssl ];
   homepage = "http://www.clckwrks.com/";
   description = "A secure, reliable content management system (CMS) and blogging platform";
   license = stdenv.lib.licenses.bsd3;
--- shell.nix
+++ shell.nix
@@ -1,36 +1,47 @@
-with (import <nixpkgs> {}).pkgs;
-let pkg = haskellngPackages.callPackage
-            ({ mkDerivation, acid-state, aeson, aeson-qq, attoparsec, base
-             , blaze-html, bytestring, cereal, containers, directory, filepath
-             , happstack-authenticate, happstack-hsp, happstack-jmacro
-             , happstack-server, happstack-server-tls, hsp, hsx-jmacro, hsx2hs
-             , ixset, jmacro, lens, mtl, network, network-uri, old-locale
-             , openssl, process, random, reform, reform-happstack, reform-hsp
-             , safecopy, stdenv, stm, tagsoup, text, time, time-locale-compat
-             , unordered-containers, utf8-string, uuid, vector, web-plugins
-             , web-routes, web-routes-happstack, web-routes-hsp, web-routes-th
-             , xss-sanitize, userid
-             }:
-             mkDerivation {
-               pname = "clckwrks";
-               version = "0.23.6";
-               src = ./.;
-               buildDepends = [
-                 acid-state aeson aeson-qq attoparsec base blaze-html bytestring
-                 cereal containers directory filepath happstack-authenticate
-                 happstack-hsp happstack-jmacro happstack-server
-                 happstack-server-tls hsp hsx-jmacro hsx2hs ixset jmacro lens mtl
-                 network network-uri old-locale process random reform
-                 reform-happstack reform-hsp safecopy stm tagsoup text time
-                 time-locale-compat unordered-containers utf8-string uuid vector
-                 web-plugins web-routes web-routes-happstack web-routes-hsp
-                 web-routes-th xss-sanitize userid
-               ];
-               buildTools = [ hsx2hs ];
-               extraLibraries = [ openssl ];
-               homepage = "http://www.clckwrks.com/";
-               description = "A secure, reliable content management system (CMS) and blogging platform";
-               license = stdenv.lib.licenses.bsd3;
-             }) {};
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, acid-state, aeson, aeson-qq, attoparsec, base
+      , blaze-html, bytestring, cereal, containers, directory, filepath
+      , happstack-authenticate, happstack-hsp, happstack-jmacro
+      , happstack-server, happstack-server-tls, hsp, hsx-jmacro, hsx2hs
+      , ixset, jmacro, lens, mtl, network, network-uri, old-locale
+      , openssl, process, random, reform, reform-happstack, reform-hsp
+      , safecopy, stdenv, stm, tagsoup, text, time, time-locale-compat
+      , unordered-containers, userid, utf8-string, uuid-orphans
+      , uuid-types, vector, web-plugins, web-routes, web-routes-happstack
+      , web-routes-hsp, web-routes-th, xss-sanitize
+      }:
+      mkDerivation {
+        pname = "clckwrks";
+        version = "0.23.10";
+        src = ./.;
+        libraryHaskellDepends = [
+          acid-state aeson aeson-qq attoparsec base blaze-html bytestring
+          cereal containers directory filepath happstack-authenticate
+          happstack-hsp happstack-jmacro happstack-server
+          happstack-server-tls hsp hsx-jmacro hsx2hs ixset jmacro lens mtl
+          network network-uri old-locale process random reform
+          reform-happstack reform-hsp safecopy stm tagsoup text time
+          time-locale-compat unordered-containers userid utf8-string
+          uuid-orphans uuid-types vector web-plugins web-routes
+          web-routes-happstack web-routes-hsp web-routes-th xss-sanitize
+        ];
+        librarySystemDepends = [ openssl ];
+        homepage = "http://www.clckwrks.com/";
+        description = "A secure, reliable content management system (CMS) and blogging platform";
+        license = stdenv.lib.licenses.bsd3;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+
 in
-  pkg.env
+
+  if pkgs.lib.inNixShell then drv.env else drv
