49e0b5bbf5885560157f6719432357ec0e8cef04
Jeremy Shaw <jeremy@n-heptane.com>
2020-09-18 13:24:14 -0400

    added Clckwrks.Authenticate.API.insecureUpdateUser

--- Clckwrks/Authenticate/API.hs
+++ Clckwrks/Authenticate/API.hs
@@ -11,12 +11,12 @@ import Control.Monad                (join)
 import Control.Monad.State          (get)
 import Control.Monad.Trans          (liftIO)
 import Clckwrks.Authenticate.Plugin (AcidStateAuthenticate(..), authenticatePlugin)
-import Data.Acid as Acid            (AcidState, query)
+import Data.Acid as Acid            (AcidState, query, update)
 import Data.Maybe                   (maybe)
 import Data.Monoid                  (mempty)
 import Data.Text                    (Text)
 import Data.UserId                  (UserId)
-import Happstack.Authenticate.Core  (GetUserByUserId(..), Email(..), User(..), Username(..))
+import Happstack.Authenticate.Core  (GetUserByUserId(..), Email(..), UpdateUser(..), User(..), Username(..))
 import Web.Plugins.Core             (Plugin(..), When(Always), addCleanup, addHandler, addPluginState, getConfig, getPluginRouteFn, getPluginState, getPluginsSt, initPlugin)
 
 getUser :: UserId -> Clck url (Maybe User)
@@ -24,6 +24,16 @@ getUser uid =
   do p <- plugins <$> get
      ~(Just (AcidStateAuthenticate authenticateState)) <- getPluginState p (pluginName authenticatePlugin)
      liftIO $ Acid.query authenticateState (GetUserByUserId uid)
+
+-- | Update an existing 'User'. Must already have a valid 'UserId'.
+--
+-- no security checks are performed to ensure that the caller is
+-- authorized to change data for the 'User'.
+insecureUpdateUser :: User -> Clck url ()
+insecureUpdateUser user =
+  do p <- plugins <$> get
+     ~(Just (AcidStateAuthenticate authenticateState)) <- getPluginState p (pluginName authenticatePlugin)
+     liftIO $ Acid.update authenticateState (UpdateUser user)
 
 getUsername :: UserId -> Clck url (Maybe Username)
 getUsername uid =
